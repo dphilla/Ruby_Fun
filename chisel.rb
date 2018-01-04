c77943639dc830f5a2acd0689d8472e4acc9ecc9
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-04 14:00:58 -0700

    Add some stuff to get started with chisel

---
+++ chisel.rb
@@ -0,0 +1,9 @@
+class Chisel
+
+  def single_line_paragraph(string)
+    ("<p>") + string + ("</p>")
+  end
+
+  #<p>This is the first line of the paragraph.</p>
+
+end
---
+++ chisel_runner.rb
@@ -0,0 +1,29 @@
+require_relative 'chisel'
+
+
+chisel = Chisel.new
+
+#puts chisel.single_line_paragraph("This is the first line of the paragraph.")
+
+
+
+
+
+
+
+input = '# My Life in Desserts\n\n## Chapter 1: The Beginning\n\n\"You just *have* to try the cheesecake,\" he said. \"Ever since it appeared in\n**Food & Wine** this place has been packed every night.\""'
+
+new_arr = input.\
+
+puts new_arr
+
+
+
+
+
+
+
+
+#chisel.single_line_paragraph("This is the first line of the paragraph.")
+
+#chisel.header_h1("#My Life in Desserts")
